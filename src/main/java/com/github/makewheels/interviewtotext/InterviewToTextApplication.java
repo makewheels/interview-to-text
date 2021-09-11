cdaa1a50c69824b1addc93a80d10825588515df7
makewheels <makewheels>
2021-09-11 14:29:43 +0800

    能获取腾讯云文件

---
+++ src/main/java/com/github/makewheels/interviewtotext/InterviewToTextApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.interviewtotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewToTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewToTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/interviewtotext/InterviewToTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.interviewtotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewToTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
